4e0b4aa14d721d436bdcc5faf3d6587c405b34f0
Ahmed Yasser Elabassy <ahmedabassy08@gmail.com>
2026-02-07 04:16:13 +0200

    Add files via upload

---
+++ assignment1/assignment1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount acc1 = new BankAccount("Ahmed Yasser", "12345678901234", "01012345678", "Cairo", 25000);
+
+            BankAccount acc2 = new BankAccount("Aly el-abassy", "98765432109876", "01123456789", "Giza");
+
+            acc1.ShowAccountDetails();
+            acc2.ShowAccountDetails();
+        }
+
+    }
+}
+
+
