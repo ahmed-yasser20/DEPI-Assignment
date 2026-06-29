001ee4c1724c29ce7f0561703d044bea2ce85024
Ahmed Yasser Elabassy <ahmedabassy08@gmail.com>
2026-06-29 06:44:46 +0300

    Delete Services directory

--- Services/Interfaces/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using BookStoreAPI.Models.DTOs.Auth;
-
-namespace BookStoreAPI.Services.Interfaces;
-
-public interface IAuthService
-{
-    Task<AuthResponse> RegisterAsync(RegisterRequest request);
-    Task<AuthResponse> LoginAsync(LoginRequest request);
-}
--- Services/Interfaces/IJwtService.cs
+++
@@ -1,9 +0,0 @@
-using BookStoreAPI.Models.Entities;
-
-namespace BookStoreAPI.Services.Interfaces;
-
-public interface IJwtService
-{
-    string GenerateToken(AppUser user, IList<string> roles);
-    DateTime GetExpiry();
-}
